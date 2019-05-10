1e62f7c44e99a2838acde6f462fd33ed4f8c9951
Hadi Moshayedi <hadi@moshayedi.net>
2019-05-10 06:37:24 -0700

    draw dice pad

--- gui.rkt
+++ gui.rkt
@@ -33,7 +33,7 @@
      [callback (lambda (row col) (print col))])
 
 
-(define (draw-dice dc x y n)
+(define (draw-dice dc x y color n)
   (define dots
     `(((32 32))
       ((21 21) (43 43))
@@ -41,25 +41,31 @@
       ((17 17) (47 47) (17 47) (47 17))
       ((15 15) (49 49) (15 49) (49 15) (32 32))
       ((17 19) (47 45) (17 45) (47 19) (32 19) (32 45))))
-  (send dc set-pen "black" 5 'solid)
+  (send dc set-pen color 5 'solid)
   (send dc set-smoothing 'aligned)
-  (send dc draw-rounded-rectangle (+ x 2) (+ x 2) 64 64 4)
+  (send dc draw-rounded-rectangle (+ x 2) (+ y 2) 64 64 4)
   (for ([p (list-ref dots (- n 1))])
-    (define x (- (first p) 2))
-    (define y (- (second p) 2))
-    (send dc draw-rounded-rectangle x y 7 7 8))
+    (define dx (- (first p) 2))
+    (define dy (- (second p) 2))
+    (send dc draw-rounded-rectangle (+ x dx) (+ y dy) 7 7 8))
   )
-(define dice-view%
+
+(define dice-pad%
   (class canvas%
     (inherit get-dc)
+    (init-field dices)
     (define/override (on-paint)
       (define dc (get-dc))
-      (draw-dice dc 0 0 6)
-      )
-    (super-new [min-height 256]
-               [min-width 256])))
+      (for ([dice dices]
+            [dx (range 2 350 70)])
+        (define color (if (car dice) "red" "black"))
+        (draw-dice dc dx 2 color (cdr dice))
+      ))
+    (super-new [min-height 80]
+               [min-width 352])))
 
-(new dice-view%
-     [parent application-frame])
+(new dice-pad%
+     [parent application-frame]
+     [dices `((#f . 1) (#f . 2) (#t . 3) (#f . 4) (#f . 5) (#f . 6))])
 
 (send application-frame show #t)
